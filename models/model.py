2fa69a9cf2185fa0fe739389642334738baa97d7
piginzoo <piginzoo@gmail.com>
2021-09-30 17:50:48 +0800

    debuging

--- models/model.py
+++ models/model.py
@@ -26,7 +26,7 @@ class Net(nn.Module):
     def __init__(self, type, device, config):
         super(Net, self).__init__()
 
-        if type == "mnist":
+        if type.startswith("mnist"):
             logger.info("使用预训练的模型Resnet18")
             resnet_model = models.resnet18(pretrained=True)
             num_classes = 10
@@ -42,21 +42,35 @@ class Net(nn.Module):
         kernel_size = size // 32  # resnet最后都是缩小32倍，无论resnet18还是resnet50
 
         self.extract_2dim_layer = None
-        if type == "mnist":
+        if type == "mnist.ce":
             self.extract_layer = nn.Sequential(
                 nn.Linear(kernel_size * kernel_size * 512, 2),  # 163万个参数/resnet18是1100万个参数
                 nn.BatchNorm1d(2))
             self.metric_fc = nn.Sequential(
                 nn.Linear(2, num_classes),
                 nn.BatchNorm1d(num_classes))
-            logger.info("构建可视化用模型（输出2维）")
+            logger.info("构建验证MNIST数据集的交叉熵模型")
+        if type == "mnist.arcface":
+            self.extract_layer = nn.Sequential(
+                nn.Linear(kernel_size * kernel_size * 512, 2),  # 163万个参数/resnet18是1100万个参数
+                nn.BatchNorm1d(2))
+            self.metric_fc = ArcMarginProduct(64,
+                                              num_classes,
+                                              s=30,
+                                              m=0.5,
+                                              easy_margin=config.easy_margin,
+                                              device=device)
+            logger.info("构建验证MNIST数据集的Arcface模型")
         else:
             self.extract_layer = nn.Identity()
             # arcface里面包含了weight，类上面的Linear的weight
-            self.metric_fc = ArcMarginProduct(512, num_classes, s=30, m=0.5,
+            self.metric_fc = ArcMarginProduct(512,
+                                              num_classes,
+                                              s=30,
+                                              m=0.5,
                                               easy_margin=config.easy_margin,
                                               device=device)
-            logger.info("构建正常模型（输出512维）")
+            logger.info("构建人脸的Arcface模型")
 
     def __num_flat_features(self, x):
         size = x.size()[1:]  # all dimensions except the batch dimension
